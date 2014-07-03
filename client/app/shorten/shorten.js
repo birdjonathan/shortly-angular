8ca6e386f31b4291c7c22cdad6669e4711d45dfc
Jonathan Bird <jonbird81@gmail.com>
2014-07-02 22:05:37 -0500

    All tests now passing

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,5 +2,10 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
-  $scope.addLink = function () {};
+  $scope.addLink = function (){
+    Links.addLink($scope.link).then(function (shortLink) {
+      console.log('hey we added a link');
+      $scope.loading = false;
+    });
+  };
 });
