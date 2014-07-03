cee9c4f1c127e2f257353db663c146d957c205d9
Jonathan Bird <jonbird81@gmail.com>
2014-07-02 20:47:51 -0500

    Now retrieves links when links/controller is loaded

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,5 +2,14 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
-  $scope.getLinks = function(){};
+  $scope.getLinks = function () {
+    Links.getLinks($scope.data).then(function (resp) {
+      $scope.data.links = resp.data;
+      console.log('getting Links');
+      console.dir(resp);
+    });
+  };
+
+  $scope.getLinks();
 });
+
