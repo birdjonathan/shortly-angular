c4bfe0d8e060b649db2e2c763417a497a17eed84
Jonathan Bird <jonbird81@gmail.com>
2014-07-03 21:53:09 -0500

    Refactored CSS styles of /links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -5,7 +5,15 @@ angular.module('shortly.links', [])
   $scope.getLinks = function () {
     Links.getLinks($scope.data).then(function (resp) {
       $scope.data.links = resp.data;
+      $scope.limitTitle();
     });
+  $scope.limitTitle = function(){
+    for (var i = 0; i < $scope.data.links.length; i++){
+     var wordArray = $scope.data.links[i].title.split(' ');
+     var limit = 5;
+     $scope.data.links[i].title = wordArray.slice(0,limit).join(' ');
+    }
+  }
   };
 
   $scope.getLinks();
